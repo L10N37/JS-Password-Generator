abfc00adb4c97cee6cd50a278e722544af2a8133
Lionel Sanderson <lfsanderson@hotmail.comgit config --global user.name Lionel>
2023-03-04 07:43:30 +1100

    moved all variables into function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
+
+// Write password to the #password output box
+function writePassword() {
+
 const lowerCaseLetters = ['a', 'b','c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's','t', 'u', 'v', 'w', 'x', 'y', 'z'];
 const upperCaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 const numbersZeroToNine = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
@@ -10,10 +14,6 @@ let IncNumeric=false;
 let IncSpecial=false;
 //                      //
 
-
-
-// Write password to the #password output box
-function writePassword() {
 
   let PassLength = prompt("Length of password? (8 to 128 Characters)");
   let UserSelections=[];
